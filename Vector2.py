b17407b28019ed1a532611b3ef29b0f8a138e031
LixNew <129871802+LixNew2@users.noreply.github.com>
2025-01-06 17:34:11 +0100

    06/01/2025

--- Vector2.py
+++ Vector2.py
@@ -2,9 +2,3 @@ class Vector2:
     def __init__(self,x, y):
         self.x = x
         self.y = y
-
-    def edit_x(self, x):
-        self.x = x
-    
-    def edit_y(self, y):
-        self.y = y
